ec9c143f05dac05bf71d8d8cdea04650fdf25f34
sandy tian <sandytian2000@hotmail.com>
2018-05-16 19:54:26 -0400

    added bar graph

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import Chart from './Chart.js';
 
 class App extends React.Component {
-    render() {
-      return (
-        <div>
-          Hello
-        </div>
-      )
-    }
+  componentDidMount(){
+    console.log('mounted');
+    axios.get('http://api.wunderground.com/api/28cbe1ca6cde9931/history_20160405/q/CA/San_Francisco.json')
+    .then((res) => {
+      console.log(res.data);
+    })
+  }
+  render() {
+    return (
+      <div>
+        <Chart legendPosition='bottom'/>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+
+//get user input of the day of travel and location
+//pull weather history from wunderground for the day but from previous years and location of the place
+//display as 24hrs - maybe for a few years
+//add google directions
