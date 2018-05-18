5ad9dd9e0760d87e850cca6a52011511c2c88857
sandy tian <sandytian2000@hotmail.com>
2018-05-17 21:03:39 -0400

    working bar graph

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -9,12 +9,49 @@ class App extends React.Component {
     super();
     this.state = {
       locationEntered: '',
-      location: ''
-    }
+      location: '',
+      locationName: '',
+      observationHoursArray: [],
+      //chart states
+      chartData: {}
+    };
     this.changeHandler = this.changeHandler.bind(this);
     this.enterLocation = this.enterLocation.bind(this);
   }
 
+  componentWillMount(){
+    this.getChartData();
+  }
+  getChartData(){
+    this.setState({
+      chartData: {
+        labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+        datasets: [
+          {
+            label: 'Population',
+            data: [
+              617594,
+              181045,
+              153060,
+              106519,
+              105162,
+              95072
+            ],
+            backgroundColor: 'rgba(54, 162, 235, 0.6)'
+            // [
+            //   'rgba(255, 99, 132, 0.6)',
+            //   'rgba(54, 162, 235, 0.6)',
+            //   'rgba(255, 206, 86, 0.6)',
+            //   'rgba(75, 192, 192, 0.6)',
+            //   'rgba(153, 102, 255, 0.6)',
+            //   'rgba(255, 159, 64, 0.6)',
+            //   'rgba(255, 99, 132, 0.6)'
+            // ]
+          }
+        ]
+      }
+    });
+  }
 
   // componentWillMount(){
 
@@ -72,7 +109,38 @@ class App extends React.Component {
     //wunderground api
     axios.get(`http://api.wunderground.com/api/28cbe1ca6cde9931/history_20160405/geolookup/q/${cords.lat},${cords.lng}.json`)
       .then((res) => {
-        console.log(res.data);
+        // console.log(res.data);
+
+        const weatherObservationsTotal = res.data.history.observations;
+
+        //array of all the hours in the day
+        let weatherObservationsArray = [];
+        //loops through and grabs all the hour updates
+        for (let i = 0; i < weatherObservationsTotal.length; i++){
+          weatherObservationsArray.push(weatherObservationsTotal[i].date.hour)
+        };
+        
+        //array of all the temp in the hours
+        let tempPerHourArray = [];
+        //loops through and grabs all the temp for everyhour
+        for (let i = 0; i < weatherObservationsTotal.length; i++){
+          tempPerHourArray.push(weatherObservationsTotal[i].tempm)
+        };
+        // console.log(tempPerHourArray);
+        
+        //clones the object
+        const chartDataClone = Object.assign({}, this.state.chartData);
+        //updates the labels to hours
+        chartDataClone.labels = weatherObservationsArray;
+        //updates the bargraph values
+        chartDataClone.datasets[0].data = tempPerHourArray;
+        //grabs the location name 
+        const newLocationName = res.data.location.city;
+
+        this.setState ({
+          locationName: newLocationName,
+          chartData: chartDataClone
+        });
       })
   }
 
@@ -105,7 +173,7 @@ class App extends React.Component {
           <input type="text" name='place' onChange={this.changeHandler} value={this.state.location}/>
           <button onClick={this.enterLocation}>Enter your location</button>
         </form>
-        <Chart legendPosition='bottom' displayTitle='true' displayText='testinggg' />
+        <Chart chartData={this.state.chartData} legendPosition='bottom' displayTitle='true' displayText={this.state.locationName} />
       </div>
     )
   }
